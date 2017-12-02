9ecef4e0706acb14843992c215e1c32e376a1293
lorelapo <lorelapo@gmail.com>
2017-12-02 17:33:20 +0000

    First commit on the local project. Just to see if things work.

--- shift.py
+++ shift.py
@@ -13,7 +13,7 @@ cypher = shift(plaintext, key, alphabet)
 print(plaintext)
 print(cypher)
 
-cryptext="AKBP AKBP PSKFG KUC ZWJSQ KB SADSFCF LVC HVCIUVH GC AIOV CT BSL OZCHVSG HVKH VS GDSBH KZZ VWG ACBSP WB CFQSF HC CMHKWB HVSA VWG CBZP KAMWHWCB LKG HC MS KZLKPG LSZZ QFSGGSQ VS QWQ BCH OKFS TCF VWG GCZQWSFG KBQ HVS HVSKHFS QWQ BCH KAIGS VWA HVS CBZP HVWBU WB TKOH VS HVCIUVH KBPHVWBU CT LKG HC QFWJS CIH KBQ GVCL K BSL GIWH CT OZCHVSG VS VKQ K OCKH TCF SJSFP VCIF CT HVS QKP KBQ KG CBS LCIZQ GKP CT K YWBU VS WG WB VWG OKMWBSH GC CBS OCIZQ GKP CT VWA HVS SADSFCF WG WB VWG QFSGGWBU FCCA"
+cryptext="DROBO HGC YXKO EZYX G DSWO G PSCROBWGX VSFOM HSDR RSC HSPO SX G ZSQCDLO KVYCO IL DRO COG GXM OFOBL MGL RO HOXD YED PSCRSXQ GXM RO PSCROM GXM RO PSCROM GXM YXKO RO HGC CSDDSXQ HSDR RSC BYM VYYUSXQ GD DRO KVOGB HGDOB GXM RO CGD GXM RO CGD DROX RSC VSXO CEMMOXVL HOXD MYHX PGB MYHX IOVYH GXM HROX RO MBOH SD EZ GQGSX RO IBYEQRD YED G VGBQO PVYEXMOB DROX DRO PVYEXMOB CGSM DY RSW RGBU LYE PSCROBWGX S ZBGL LYE VOD WO VSFO S GW XY PVYEXMOB BOGVVL IED GX OXKRGXDOM ZBSXKO HRGD QYYM HSVV SD MY LYE DY USVV WO S CRYEVM XYD IO QYYM DY OGD ZED WO SX DRO HGDOB GQGSX GXM VOD WO QY KYWO CGSM DRO PSCROBWGX DROBO SC XY XOOM PYB CY WGXL HYBMC GIYED SD G PSCR DRGD KGX DGVU S CRYEVM KOBDGSXVL VOD QY GXLRYH GXM HSDR DRGD RO ZED RSW IGKU GQGSX SXDY DRO KVOGB HGDOB GXM DRO PVYEXMOB HOXD DY DRO IYDDYW VOGFSXQ G VYXQ CDBOGU YP IVYYM IORSXM RSW DROX DRO PSCROBWGX QYD EZ GXM HOXD RYWO DY RSC HSPO SX DRO ZSQCDLO"
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def getCount(message):
@@ -33,17 +33,24 @@ def getLetterCount(message):
     return letterCount
 
 def getFrequencyString(message):
-    lettercount=getLetterCount(message)
-    countstring=[]
-    for letter in LETTERS:
-        countstring.append("letter, lettercount[letter]")
-    return countstring
+    letterCount=getLetterCount(message)
+    sortedDict = sorted(letterCount.items(), key=operator.itemgetter(1))
+    sortedDict.reverse()
+    return sortedDict
 
+def getAlphabet(freqString):
+    return ''.join([e[0] for e in freqString])
 
 englishLetterFreq = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97, 'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25, 'L': 4.03, 'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97, 'P': 1.93, 'B': 1.29, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10, 'Z': 0.07}
-ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
-frequencies=getFrequencyString(cryptext)
+etaoin = ' etaoinshrdlcumwfgypbvkjxqz'
+frequencies = getFrequencyString(cryptext)
 print(frequencies)
+newalphabet = getAlphabet(frequencies).lower()
+print(cryptext)
+print(' '+newalphabet)
+print(etaoin)
+decoded = shift(cryptext, etaoin, ' '+newalphabet)
+print(decoded)
 
 
 
