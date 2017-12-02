38c7b8a4e86a3a830825785e8c15ce0d225b401e
lorelapo <lorelapo@gmail.com>
2017-12-02 18:43:48 +0000

    Removed some dirt from shift.py. I am starting to code the trigrams analysis function.

--- shift.py
+++ shift.py
@@ -21,13 +21,6 @@ def getCount(sInput):
         count +=1
     return count
 
-# def getCount(message):
-#     count=0
-#     for letter in message.upper():
-#         if letter in LETTERS:
-#             count +=1
-#     return count
-
 def getLetterCount(sInput):
     totCount = getCount(sInput)
     letCount = {'a' : 0}
@@ -36,16 +29,17 @@ def getLetterCount(sInput):
             letCount[e] += 100 / totCount
         else:
             letCount[e] = 100 / totCount
-    return letCount
+        return letCount
 
-# def getLetterCount(message):
-#     totalcount=getCount(message)
-#     letterCount = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0,
-#                  'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
-#     for letter in message.upper():
-#         if letter in LETTERS:
-#             letterCount[letter] += 100/totalcount
-#     return letterCount
+def getTrigramCount(sInput):
+    totCount = getCount(sInput)-2
+    letCount = {'a': 0}
+    for e in sInput.lower():
+        if e in letCount.keys():
+            letCount[e] += 100 / totCount
+        else:
+            letCount[e] = 100 / totCount
+    return letCount
 
 def getFrequencyString(message):
     letterCount = getLetterCount(message)
@@ -87,12 +81,4 @@ decData = shift(encData, etaoin, freqEncAlph)
 
 print(langdetect.detect_langs(decData), decData)
 
-# newalphabet = getAlphabet(frequencies).lower()
-# print(cryptext)
-# print(' '+newalphabet)
-# print(etaoin)
-# decoded = shift(cryptext, etaoin, ' '+newalphabet)
-# print(decoded)
-#
-#
-#
+
